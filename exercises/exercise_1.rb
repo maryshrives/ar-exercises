ff5d495e493c94d498dffe92b268ede9ee65d2b9
Mary Shrives <mshrives@alumni.yorku.ca>
2018-01-16 17:31:12 +0000

    did setup, first exercise

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,33 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+  # * Burnaby (annual_revenue of 300000, carries men's and women's apparel)
+  # * Richmond (annual_revenue of 1260000 carries women's apparel only)
+  # * Gastown (annual_revenue of 190000 carries men's apparel only)
+
+class Store < ActiveRecord::Base
+  s = Store.create (
+    [
+      {
+        name: "Burnaby",
+        annual_revenue: 300_000,
+        mens_apparel: true,
+        womens_apparel: true
+      },
+      {
+        name: "Richmond",
+        annual_revenue: 1_260_000,
+        mens_apparel: false,
+        womens_apparel: true
+      },
+      {
+        name: "Gastown",
+        annual_revenue: 190_000,
+        mens_apparel: true,
+        womens_apparel: false
+      }
+    ]
+  )
+  puts Store.count
+end
